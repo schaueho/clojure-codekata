47d73e3735ee72c83204e706cca03d0dfa1ad4b2
Holger Schauer <Holger.Schauer@gmx.de>
2013-03-24 20:14:23 +0100

    kata2: first working version

--- kata2-chop-binary-search/src/kata2_chop_binary_search/core.clj
+++ kata2-chop-binary-search/src/kata2_chop_binary_search/core.clj
@@ -1,17 +1,11 @@
 (ns kata2-chop-binary-search.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
-
-(defn chop [value array]
-  -1)
-
-; (defn chop [x coll]
-;   (let [result (keep-indexed 
-;                 (fn [idx item]
-;                   (if (= item x) idx))
-;                 coll)]
-;     (if result
-;       (first result))))
+(defn chop [x coll]
+  (let [result
+        (keep-indexed
+         (fn [idx item]
+           (when (= item x) idx))
+         coll)]
+    (if (empty? result)
+      -1
+      (first result))))
