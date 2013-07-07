63631b9962c4435a0dac58f1130ca78052c090d8
Holger Schauer <Holger.Schauer@gmx.de>
2013-07-07 13:19:21 +0200

    First working solution for kata4 data munging

--- kata4-data-munging/src/kata4_data_munging/core.clj
+++ kata4-data-munging/src/kata4_data_munging/core.clj
@@ -1,6 +1,55 @@
 (ns kata4-data-munging.core)
+(require ['clojure.java.io :as 'io])
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn string-to-int [string]
+  "Parses a consecutive set of numbers into an integer or return nil"
+  (try
+    (Integer/parseInt (re-find #"\d+" string))
+    (catch Exception e nil)))
+
+(defn first-word [string]
+  "Returns first consecutive non-whitespace chars from string"
+  (re-find #"\S+" string))
+
+ 
+(defn parse-line [line pattern]
+  (let [parts pattern]
+    (loop [remkeys (keys parts) linemap {}]
+      (if (empty? remkeys)
+        linemap
+        (let [key (first remkeys)
+              [start end parsefn] (get parts key)
+              value (parsefn (try
+                               (subs line start end)
+                               (catch Exception e nil)))]
+          (if value
+            (recur (rest remkeys)
+                   (conj linemap
+                         (hash-map key value)))
+            ; silently skip any parsing errors
+            nil))))))    
+
+(def day-pattern
+  ;this pattern is not complete and could be extended
+  (hash-map :day [1 4 #(first-word %)]
+            :MxT [5 8 #(string-to-int %)]
+            :MnT [9 14 #(string-to-int %)]
+            :AvT [15 20 #(string-to-int %)]))
+                                       
+(defn parse-day [line]
+  "Parse a day from a line"
+  (parse-line day-pattern))
+
+      
+(defn find-lowest-temperature [weatherfile]
+  "Return day in weatherfile with the smallest temperature spread"
+  (loop [lines (line-seq (io/reader weatherfile)) minday 0 minspread 0]
+    (if (empty? lines)
+      minday
+      (let [{mnt :MnT mxt :MxT curday :day} (parse-day (first lines))            
+            curspread (when (and mnt mxt) (- mxt mnt))]
+        (if (and curday curspread
+                 (or (= minspread 0)
+                     (< curspread minspread)))
+          (recur (next lines) curday curspread)
+          (recur (next lines) minday minspread))))))  
--- kata4-data-munging/test/kata4_data_munging/core_test.clj
+++ kata4-data-munging/test/kata4_data_munging/core_test.clj
@@ -1,11 +1,26 @@
 (ns kata4-data-munging.core-test
   (:use clojure.test
-        kata3-data-munging.core))
+        kata4-data-munging.core))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest string-to-int-returns-number
+  (is (= 1 (string-to-int "  1")))
+  (is (= 99 (string-to-int "  99 abcde"))))
 
-(deftest addition
-  (is (= 3 (+ 2 1))))
+(deftest string-to-int-returns-nil-without-number
+  (is (= nil (string-to-int "  a"))))
 
+(deftest first-word-returns-non-whitespace-from-string
+  (is (= "foo" (first-word "  foo bar "))))
+
+(deftest parse-day-generates-data
+  (let [testline
+        "   1  88    59    74          53.8       0.00 F       280  9.6 270  17  1.6  93 23 1004.5"]
+ (is {:AvT 74, :day "1", :MxT 88, :MnT 59} (parse-day testline))))
+
+(deftest parse-day-returns-nil-for-nonday-data
+  (is (= nil (parse-day "")))
+  (is (= nil (parse-day "This is some weird line"))))
+
+(deftest find-lowest-temperature-yields-result
+  (let [weather-file "/home/schauer/programming/clojure/codekata/kata4-data-munging/weather.dat"]
+    (is "14" (find-lowest-temperature weather-file))))
