55764be7ab99700634e17d2f27717b6bbf48da27
Holger Schauer <Holger.Schauer@gmx.de>
2013-08-13 11:21:15 +0200

    kata5: Add some dependencies to the lein project

--- kata5-bloom-filters/project.clj
+++ kata5-bloom-filters/project.clj
@@ -1,6 +1,7 @@
 (defproject kata5-bloom-filters "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Solving the bloom filter kata in Clojure"
+  :url "https://github.com/schaueho/clojure-codekata/tree/master/kata5-bloom-filters"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.5.0"]
+                 [org.clojure/tools.nrepl "0.2.3"]])
