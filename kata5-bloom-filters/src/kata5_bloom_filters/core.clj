0520e83439fffb88a15dc0c9310f6cdc649b6b15
Holger Schauer <Holger.Schauer@gmx.de>
2013-08-13 11:21:36 +0200

    kata5: Add some hash functions

--- kata5-bloom-filters/src/kata5_bloom_filters/core.clj
+++ kata5-bloom-filters/src/kata5_bloom_filters/core.clj
@@ -1,6 +1,49 @@
 (ns kata5-bloom-filters.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+; cf. http://www.cse.yorku.ca/~oz/hash.html
+(defn sum-chars 
+  "Sum up the chars of a given string"
+  ; this is essentially K&R "lose-lose" pretty bad hashing algorithm
+  [charseq]
+  (reduce + (map int charseq)))
+
+(defn djb-string-hash
+  "Use djb's method for hashing a string"
+  [charseq]
+  (reduce (fn [curhash charval]
+            ^long (unchecked-add 
+                   (unchecked-add (bit-shift-left curhash 5) curhash) 
+                   charval))
+          (cons 5381 (map int charseq))))
+
+(defn- comp-hash-val
+  [curhash charval]
+  (unchecked-subtract 
+   ^long (unchecked-add
+          charval
+          (unchecked-add (bit-shift-left curhash 16) (bit-shift-left curhash 6)))
+   (unchecked-long curhash)))
+
+(defn sdbm-string-hash
+  "Use the method from sdbm for hashing a string"
+  [charseq]
+  (reduce comp-hash-val
+          (cons  0 (map int charseq))))
+
+(defn sdbm-hash-recur 
+  "Recursive version of the sdbm hash"
+  ;; Compute hash(i) = hash(i - 1) * 65599 + str[i]; 
+  ([charseq] (sdbm-hash-recur charseq 0))
+  ([charseq ^long acc]
+     (if (empty? charseq)
+       acc
+       (recur (rest charseq) 
+              (unchecked-add (unchecked-multiply acc 65599) 
+                             (int (first charseq)))))))
+  
+; cf. http://www.isthe.com/chongo/tech/comp/fnv/
+(defn fnv-hash [charseq]
+  (let [fnv-prime 0x811C9DC5]
+    (reduce (fn [^long curhash charval]
+              ^long (bit-xor (unchecked-multiply curhash fnv-prime) charval))
+            (cons 0 (map (comp unchecked-long int) charseq)))))
--- kata5-bloom-filters/test/kata5_bloom_filters/core_test.clj
+++ kata5-bloom-filters/test/kata5_bloom_filters/core_test.clj
@@ -2,6 +2,18 @@
   (:use clojure.test
         kata5-bloom-filters.core))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest sum-of-chars-returns-valid-result
+  (testing "Testing hash function sum-chars"
+    (is (= 633 (sum-chars "foobar")))))
+
+(deftest djb-string-hash-returns-valid-result
+  (testing "Testing hash function djb-string-match"
+    (is (= 6953516687550 (djb-string-hash "foobar")))))
+
+(deftest sdbm-string-hash-returns-valid-result
+  (testing "Testing hash function sdbm-string-hash"
+    (is (= (sdbm-hash-recur "foobar") (sdbm-string-hash "foobar")))))
+
+(deftest fnvhash-hash-returns-valid-result
+  (testing "Testing hash function fnv-hash"
+    (is (= 6157838686567520599 (fnv-hash "foobar")))))
