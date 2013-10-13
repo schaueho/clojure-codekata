1481a04239f91c6844f77ec383216f92d93ec0d5
Holger Schauer <holger.schauer@gmx.de>
2013-10-13 16:38:22 +0200

    kata5: use local wordlist.txt for test instead of the unix standard wordlist.

--- kata5-bloom-filters/test/kata5_bloom_filters/core_test.clj
+++ kata5-bloom-filters/test/kata5_bloom_filters/core_test.clj
@@ -19,13 +19,18 @@
   (testing "Testing hash function fnv-hash"
     (is (= 6157838686567520599 (fnv-hash "foobar")))))
 
+(deftest optimal-size-calculates-value
+  (testing "Optimal size calculation for bloom filter"
+    (is (= 313751.0 (optimal-size 45420 1/1000)))))
+
+
 (deftest bloom-added-values-are-contained
   (testing "An added value is contained in the resulting bloom filter"
     (is (bloom-contains? (bloom-add (BitSet. 100000) "foobar") "foobar"))))
 
 (deftest bloom-of-dict-words-finds-data
   (testing "A fully build bloom filter finds the data that got added"
-    (let [bloom (build-bloom "/usr/share/dict/words" :size 921999)]
-      (is (bloom-contains? bloom "ABM's"))
-      (is (bloom-contains? bloom "Anthony"))
+    (let [bloom (build-bloom "wordlist.txt" :size 313751)]
+      (is (bloom-contains? bloom "aback"))
+      (is (bloom-contains? bloom "abandons"))
       (is (not (bloom-contains? bloom "foo"))))))
