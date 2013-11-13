1a7686ae565fc0e9c66ec2748c510594b170677b
Holger Schauer <Holger.Schauer@gmx.de>
2013-11-13 13:42:53 +0100

    kata6: solve the kata, using Panditas algorithm

--- kata6-anagrams/src/kata6_anagrams/core.clj
+++ kata6-anagrams/src/kata6_anagrams/core.clj
@@ -25,5 +25,83 @@
         chars2 (word-to-charmap (remove-blanks word2))]
     (= chars1 chars2)))
 
+(defn find-largest-index-with-bigger-successor [squence]
+  (loop [restsq (seq squence)
+         curpos 0
+         curresult nil]
+    (cond (or (empty? restsq)
+              (empty? (rest restsq)))
+          curresult
+          ((comp pos? compare) (second restsq) (first restsq))
+          (recur (rest restsq)
+                 (inc curpos)
+                 curpos)
+          :else
+          (recur (rest restsq)
+                 (inc curpos)
+                 curresult))))
 
 
+(defn find-largest-index-bigger-value [squence index]
+  (let [compval (nth (vec squence) index)]
+    (loop [restsq (seq squence)
+           curpos 0
+           curresult nil]
+    (cond (empty? restsq)
+          curresult
+          ((comp pos? compare) (first restsq) compval)
+          (recur (rest restsq)
+                 (inc curpos)
+                 curpos)
+          :else
+          (recur (rest restsq)
+                 (inc curpos)
+                 curresult)))))
+
+(defn swap-positions [squence k l]
+  (let [seqvec (vec squence)]
+    (assoc (assoc seqvec k (nth seqvec l))
+      l (nth seqvec k))))
+
+(defn reverse-tail [squence tail-position]
+  (let [prefix (take tail-position squence)
+        tail (drop tail-position squence)
+        revtail (reverse tail)]
+    (concat prefix revtail)))
+
+(defn next-permutation [squence]
+  (when-let [k (find-largest-index-with-bigger-successor squence)]
+    (let [l (find-largest-index-bigger-value squence k)
+          swapped (swap-positions squence k l)
+          current-perm (reverse-tail swapped (inc k))]
+      current-perm)))
+
+(defn generate-permutations [squence]
+  ;; generate permutations in lexicographic order, following Naranya Pandita, 14th century
+  ;; cf. https://en.wikipedia.org/wiki/Permutation
+  (let [start-perm (sort squence)]
+    (loop [permutation (next-permutation start-perm)
+           result (list start-perm)]
+      (if (or (not permutation)
+              (empty? permutation))
+        result
+        (recur (next-permutation permutation)
+               (concat result (list permutation)))))))
+
+(defn generate-anagrams [word]
+  "Generate all anagrams of word"
+  (map (partial apply str) (generate-permutations word)))
+
+(defn find-anagrams [word words]
+  "Finds all anagrams of word in (the sequence of) words"
+  (let [anagrams (generate-anagrams word)
+        wordset (set words)]
+    (loop [candidates anagrams
+           result []]
+      (if (empty? candidates)
+        result
+        (recur (rest candidates)
+               (if (and (not (= (first candidates) word))
+                        (contains? wordset (first candidates)))
+                 (concat result (list (first candidates)))
+                 result))))))
--- kata6-anagrams/test/kata6_anagrams/core_test.clj
+++ kata6-anagrams/test/kata6_anagrams/core_test.clj
@@ -1,6 +1,6 @@
 (ns kata6-anagrams.core-test
-  (:use clojure.test
-        midje.sweet
+  (:require [clojure.string :refer [split-lines]])
+  (:use midje.sweet
         kata6-anagrams.core))
 
 (facts "Testing the set implementation for checking anagrams"
@@ -16,3 +16,54 @@
 
        (fact "anagram-hash-map fails on non-anagrams"
              (anagram-phm? "the lalalaw" "wealth") => false))
+
+(facts "Testing the implementation details of the Pandita algorithm"
+       (fact "finding the largest index with bigger successor"
+             (find-largest-index-with-bigger-successor [1 2]) => 0
+             (find-largest-index-with-bigger-successor [1 2 3 4]) => 2
+             (find-largest-index-with-bigger-successor [1 2 4 3]) => 1
+             (find-largest-index-with-bigger-successor [1 3 4 2]) => 1
+             (find-largest-index-with-bigger-successor [1 4 3 2]) => 0)
+       (fact "finding the largest index that has a bigger value than some other position"
+             (find-largest-index-bigger-value [1 2 3 4] 2) => 3
+             (find-largest-index-bigger-value [1 2 4 3] 1) => 3
+             (find-largest-index-bigger-value [1 3 4 2] 1) => 2
+             (find-largest-index-bigger-value [1 4 3 2] 0) => 3)
+       (fact "swapping two positions in a sequence"
+             (swap-positions [1 2 3 4] 2 3) => [1 2 4 3]
+             (swap-positions [1 2 4 3] 1 3) => [1 3 4 2]
+             (swap-positions [1 3 4 2] 1 2) => [1 4 3 2]
+             (swap-positions [1 4 3 2] 0 3) => [2 4 3 1])
+       (fact "reverse the tail of a sequence"
+             (reverse-tail [1 2 4 3] 2) => [1 2 3 4]
+             (reverse-tail [1 3 4 2] 1) => [1 2 4 3]
+             (reverse-tail [1 4 3 2] 0) => [2 3 4 1])
+       (fact "finding the next permutation"
+             (next-permutation [1 2 3 4]) => [1 2 4 3]
+             (next-permutation [1 2 4 3]) => [1 3 2 4])
+       (fact "generating all permutations"
+             (generate-permutations [1 2 3 4]) => '((1 2 3 4) (1 2 4 3)
+                                                    (1 3 2 4) (1 3 4 2)
+                                                    (1 4 2 3) (1 4 3 2)
+                                                    (2 1 3 4) (2 1 4 3)
+                                                    (2 3 1 4) (2 3 4 1)
+                                                    (2 4 1 3) (2 4 3 1)
+                                                    (3 1 2 4) (3 1 4 2)
+                                                    (3 2 1 4) (3 2 4 1)
+                                                    (3 4 1 2) (3 4 2 1)
+                                                    (4 1 2 3) (4 1 3 2)
+                                                    (4 2 1 3) (4 2 3 1)
+                                                    (4 3 1 2) (4 3 2 1))
+             (count (generate-permutations [1 2 3 4])) => 24))
+
+(facts "Testing the anagram implementation"
+       (fact "Generating all anagrams"
+             (generate-anagrams "ftw") => '("ftw" "fwt" "tfw" "twf" "wft" "wtf"))
+       (let [words (split-lines (slurp "wordlist.txt"))]
+             (find-anagrams "kinship" words) => '("pinkish")
+             (find-anagrams "enlist" words) => '("inlets" "listen" "silent")
+             (find-anagrams "boaster" words) => '("boaters" "borates")
+             (find-anagrams "sinks" words) => '("skins")
+             (find-anagrams "knits" words) => '("stink")
+             (find-anagrams "rots" words) => '("sort")
+             (find-anagrams "thelaw" words) => '("wealth")))
