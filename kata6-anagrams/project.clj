9cc4ee2268945fac007c2946056612aadff34c55
Holger Schauer <Holger.Schauer@gmx.de>
2013-11-13 13:39:28 +0100

    kata6: add dev dependency to trace/repl

--- kata6-anagrams/project.clj
+++ kata6-anagrams/project.clj
@@ -5,4 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.0"]
                  [org.clojure/core.logic "0.8.4"]]
-  :profiles {:dev {:dependencies [[midje "1.5.1"]]}})
+  :profiles {:dev {:dependencies [[midje "1.5.1"]
+                                  [org.clojure/tools.trace "0.7.6"]
+                                  [org.clojure/tools.nrepl "0.2.3"]]}})
