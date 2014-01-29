7ac2dbd47c6cb9f48425f16cd69073cf8bcfaa84
Holger Schauer <Holger.Schauer@gmx.de>
2014-01-29 17:45:03 +0100

    Update to core.logic 0.8.5.

--- kata6-anagrams/project.clj
+++ kata6-anagrams/project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.0"]
-                 [org.clojure/core.logic "0.8.4"]]
+                 [org.clojure/core.logic "0.8.5"]]
   :profiles {:dev {:dependencies [[midje "1.5.1"]
                                   [org.clojure/tools.trace "0.7.6"]
                                   [org.clojure/tools.nrepl "0.2.3"]]}})
