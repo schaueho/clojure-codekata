b60d0f3728792635bc711862793ef8837bebfb79
holger.schauer <holger.schauer@cls-communication.com>
2014-06-10 18:01:24 +0200

    Add kata14 directory.

---
+++ kata14-trigrams/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to kata14-trigrams
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ kata14-trigrams/test/kata14_trigrams/core_test.clj
@@ -0,0 +1,7 @@
+(ns kata14-trigrams.core-test
+  (:use clojure.test
+        kata14-trigrams.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
